ff722b1d85471c027c42914838dddf7443e62de8
dkandrievsky@luxoft.com <dkandrievsky@luxoft.com>
2021-09-14 14:03:59 +0300

    user settings usage

---
+++ WorklogWebApp/WorklogWebAssembly/Client/Data/ShowEncryptedOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorklogWebAssembly.Client.Data
+{
+    public class ShowEncryptedOptions
+    {
+        public bool decrypt { get; set; } = false;
+        public bool showEncrypted { get; set; } = false;
+        public string key { get; set; } = "";
+
+        public ShowEncryptedOptions(bool decrypt, bool showEncrypted, string key)
+        {
+            this.decrypt = decrypt;
+            this.showEncrypted = showEncrypted;
+            this.key = key;
+        }
+        public ShowEncryptedOptions Clone()
+        {
+            return new ShowEncryptedOptions(decrypt, showEncrypted, key);
+        }
+    }
+}
