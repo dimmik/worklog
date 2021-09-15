41c56176c241451aedb7ab098a3b0eaad592fbca
dkandrievsky@luxoft.com <dkandrievsky@luxoft.com>
2021-09-15 17:26:49 +0300

    keep last wakeups

--- WorklogWebApp/WorklogWebAssembly/Server/Svc/StartupInfo.cs
+++ WorklogWebApp/WorklogWebAssembly/Server/Svc/StartupInfo.cs
@@ -8,6 +8,15 @@ namespace WorklogWebAssembly.Server.Svc
     public class StartupInfo
     {
         public DateTimeOffset StartTime { get; set; } = DateTimeOffset.Now.ToOffset(TimeSpan.FromHours(3));
-        public DateTimeOffset LastWakeup { get; set; } = DateTimeOffset.Now.ToOffset(TimeSpan.FromHours(3));
+        public List<DateTimeOffset> LastWakeups { get; set; } = new List<DateTimeOffset>();
+        public int numberWakeupsToKeep = 15;
+        public void Wakeup()
+        {
+            if (LastWakeups.Count >= numberWakeupsToKeep)
+            {
+                LastWakeups.RemoveAt(0);
+            }
+            LastWakeups.Add(DateTimeOffset.Now.ToOffset(TimeSpan.FromHours(3)));
+        }
     }
 }
