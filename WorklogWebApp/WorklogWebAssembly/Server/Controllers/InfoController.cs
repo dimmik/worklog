a18e1e0aaa985479916f8500b3a70c4af30612ae
dkandrievsky@luxoft.com <dkandrievsky@luxoft.com>
2021-09-15 10:35:06 +0300

    startup info

---
+++ WorklogWebApp/WorklogWebAssembly/Server/Controllers/InfoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorklogWebAssembly.Server.Svc;
+
+namespace WorklogWebAssembly.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        private readonly StartupInfo startupInfo;
+
+        public InfoController(StartupInfo startupInfo)
+        {
+            this.startupInfo = startupInfo;
+        }
+
+        [HttpGet("start")]
+        public DateTimeOffset GetStartupTime()
+        {
+            return startupInfo.StartTime;
+        }
+    }
+}
